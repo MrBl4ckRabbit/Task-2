ebc800a4440034ff4b1432f470dda00e1f8b9162
MrBl4ckRabbit <mr.bl4ck.rabbit@gmail.com>
2024-08-22 01:49:30 +0300

    complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld newBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(newBean.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getMessage());
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        System.out.println("Is bean equals newBean: " + bean.equals(newBean));
+        System.out.println("Is cat1 equals cat2: " + cat1.equals(cat2));
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,16 +1,11 @@
-import org.springframework.stereotype.Component;
-
-
-public class HelloWorld {
- 
+public class Cat {
     private String message;
- 
+
     public String getMessage() {
         return message;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
-     
 }
